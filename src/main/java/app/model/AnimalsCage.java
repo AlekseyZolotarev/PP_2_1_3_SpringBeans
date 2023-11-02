f984f0968f62b79645808cdeec0ac9788dee3c67
Aleksey Zolotarev <alexiy_54@mail.ru>
2023-11-02 22:45:48 +0300

    2

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -1,19 +1,31 @@
 package app.model;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AnimalsCage {
+    private final Animal animal;
+    private final ApplicationContext applicationContext =
+            new AnnotationConfigApplicationContext(Timer.class);
 
     @Autowired
-    private Animal animal;
+    public AnimalsCage(@Qualifier("dog") Animal animal) {
+        this.animal = animal;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
         System.out.println(animal.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(getTimer().getTime());
         System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return (Timer) applicationContext.getBean("timer");
     }
 }
